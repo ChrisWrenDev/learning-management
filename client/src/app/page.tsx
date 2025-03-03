cb1911b6182f9e2906bbd08c07521fff4fc7cf8a
chris wren <chriswrendeveloper@gmail.com>
2025-03-03 11:22:57 +0000

    Added Non Dashboard Landing Page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Landing from "@/app/(nondashboard)/landing/page";
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
     </div>
   );
 }
