81476e5793db2aa552ad7374ff8bad4293297979
chris wren <chriswrendeveloper@gmail.com>
2025-03-07 14:06:49 +0000

    Added User Clerk controller and routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { createClerkClient, requireAuth } from "@clerk/express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -8,6 +9,7 @@ import morgan from "morgan";
 
 /* ROUTE IMPORTS */
 import courseRoutes from "./routes/courseRoutes";
+import userClerkRoutes from "./routes/userClerkRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -15,6 +17,10 @@ const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -31,6 +37,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
