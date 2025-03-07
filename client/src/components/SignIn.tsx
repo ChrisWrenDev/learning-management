ccf0b45cfa497b1405803885ebcb8e8854dc9091
chris wren <chriswrendeveloper@gmail.com>
2025-03-07 10:04:52 +0000

    Added Redirect to Clerk Components

--- client/src/components/SignIn.tsx
+++ client/src/components/SignIn.tsx
@@ -1,10 +1,32 @@
 "use client";
 
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const SignInComponent = () => {
+  const { user } = useUser();
+  const searchParams = useSearchParams();
+  const isCheckoutPage = searchParams.get("showSignUp") !== null;
+  const courseId = searchParams.get("id");
+
+  const signUpUrl = isCheckoutPage
+    ? `/checkout?step=1&id=${courseId}&showSignUp=true`
+    : "/signup";
+
+  const getRedirectUrl = () => {
+    if (isCheckoutPage) {
+      return `/checkout?step=2&id=${courseId}&showSignUp=true`;
+    }
+
+    const userType = user?.publicMetadata?.userType as string;
+    if (userType === "teacher") {
+      return "/teacher/courses";
+    }
+    return "/user/courses";
+  };
+
   return (
     <SignIn
       appearance={{
@@ -27,6 +49,10 @@ const SignInComponent = () => {
           footerActionLink: "text-primary-750 hover:text-primary-600",
         },
       }}
+      signUpUrl={signUpUrl}
+      forceRedirectUrl={getRedirectUrl()}
+      routing="hash"
+      afterSignOutUrl="/"
     />
   );
 };
--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,10 +1,32 @@
 "use client";
 
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const SignUpComponent = () => {
+  const { user } = useUser();
+  const searchParams = useSearchParams();
+  const isCheckoutPage = searchParams.get("showSignUp") !== null;
+  const courseId = searchParams.get("id");
+
+  const signInUrl = isCheckoutPage
+    ? `/checkout?step=1&id=${courseId}&showSignUp=false`
+    : "/signin";
+
+  const getRedirectUrl = () => {
+    if (isCheckoutPage) {
+      return `/checkout?step=2&id=${courseId}&showSignUp=false`;
+    }
+
+    const userType = user?.publicMetadata?.userType as string;
+    if (userType === "teacher") {
+      return "/teacher/courses";
+    }
+    return "/user/courses";
+  };
+
   return (
     <SignUp
       appearance={{
@@ -27,6 +49,10 @@ const SignUpComponent = () => {
           footerActionLink: "text-primary-750 hover:text-primary-600",
         },
       }}
+      signInUrl={signInUrl}
+      forceRedirectUrl={getRedirectUrl()}
+      routing="hash"
+      afterSignOutUrl="/"
     />
   );
 };
